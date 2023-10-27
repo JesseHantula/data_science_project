1eb0c35b6a846878cd6ec361d7e9ab7f75f9dbb0
Lene Bierstedt <schmetti3000@web.de>
2023-10-27 17:51:43 +0300

    improving website UI

--- src/map_creation.py
+++ src/map_creation.py
@@ -11,9 +11,22 @@ def create_map():
 
     # add marker one by one on the map
     for i in range(0,len(data)):
+        html=f"""
+        <h3> {data.iloc[i]['Prediction']['Station']}</h3>
+        <ul>
+            <li>{data.iloc[i]['Prediction']['Day 1']}:</br> <font color={data.iloc[i]['Prediction']['Color 1']}>{data.iloc[i]['Prediction']['Prediction 1']}</font></li>
+            <li>{data.iloc[i]['Prediction']['Day 2']}:</br> <font color={data.iloc[i]['Prediction']['Color 2']}>{data.iloc[i]['Prediction']['Prediction 2']}</font></li>
+            <li>{data.iloc[i]['Prediction']['Day 3']}:</br> <font color={data.iloc[i]['Prediction']['Color 3']}>{data.iloc[i]['Prediction']['Prediction 3']}</font></li>
+            <li>{data.iloc[i]['Prediction']['Day 4']}:</br> <font color={data.iloc[i]['Prediction']['Color 4']}>{data.iloc[i]['Prediction']['Prediction 4']}</font></li>
+            <li>{data.iloc[i]['Prediction']['Day 5']}:</br> <font color={data.iloc[i]['Prediction']['Color 5']}>{data.iloc[i]['Prediction']['Prediction 5']}</font></li>
+        </ul>
+        """
+        iframe = folium.IFrame(html=html, width=200, height=200)
+        popup = folium.Popup(iframe, max_width=2650)
         folium.Marker(
             location=[data.iloc[i]['y'], data.iloc[i]['x']],
-            popup=data.iloc[i]['Prediction'],
+            popup=popup,
+            icon=folium.Icon(color="orange")
         ).add_to(m)
 
     # Save it as html
--- src/map_creation_prep.py
+++ src/map_creation_prep.py
@@ -8,6 +8,19 @@ we need to create a dataframe with columns of the station coordinates, and the b
 First, we do the required imports and load the coordinate csv file and the predictions csv file.
 """
 
+def get_color(busyness):
+    if busyness == "Not Busy":
+        return "#006400"
+    elif busyness == "Moderately Busy":
+        return "#FFD800"
+    elif busyness == "Quite Busy":
+        return "#F28500"
+    elif busyness == "Very Busy":
+        return "#FF0000"
+    else:
+        return "#451425"
+
+
 def get_prediction_coordinates():
 
     station_coordinates = pd.read_csv("./data/HSL/city_bike_station_coordinates.csv")
@@ -34,12 +47,22 @@ def get_prediction_coordinates():
     day4 = (dt.datetime.today() + dt.timedelta(days=4)).strftime("%d.%m.%Y")
     day5 = (dt.datetime.today() + dt.timedelta(days=5)).strftime("%d.%m.%Y")
 
-    strings = []
+    days = [day1, day2, day3, day4, day5]
+
+    dictionaries = []
     for row in predictions.iterrows():
-        string = f"{row[1]['Station']} \n {day1}: {row[1]['Predicted Busyness'][0]} \n {day2}: {row[1]['Predicted Busyness'][1]} \n {day3}: {row[1]['Predicted Busyness'][2]} \n {day4}: {row[1]['Predicted Busyness'][3]} \n {day5}: {row[1]['Predicted Busyness'][4]}"
-        strings.append(string)
+        d = {}
+        d["Station"] = row[1]['Station']
+        for i in range(5):
+            key = "Day " + str(i+1)
+            d[key] = days[i]
+            predkey = "Prediction " + str(i+1)
+            d[predkey] = row[1]['Predicted Busyness'][i]
+            colorkey = "Color " + str(i+1)
+            d[colorkey] = get_color(row[1]['Predicted Busyness'][i])
+        dictionaries.append(d)
     
-    predictions["Prediction"] = strings
+    predictions["Prediction"] = dictionaries
 
     predictions = predictions.drop(columns=["Station", "Predicted Busyness"])
 
